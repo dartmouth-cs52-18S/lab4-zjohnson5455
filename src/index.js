4b5aaba2ed92cc090a7a5e92064e6e08b0a06f16
zjohnson5455 <zacharyjohnsonri@gmail.com>
2018-04-13 18:34:50 -0400

    changed to

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 function updateTime (){
   let num = 0;
   setInterval(function() {
-    $('#main').html('You have been on this page for ${num} seconds.');
+    $('#main').html(`You have been on this page for ${num} seconds.`);
     num++;
   }, 1000);
 }
