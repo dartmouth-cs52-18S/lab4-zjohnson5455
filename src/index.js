384945884bec5e8fb826c962cd8e69f6f4b32d9a
zjohnson5455 <zacharyjohnsonri@gmail.com>
2018-04-13 18:32:08 -0400

    fixing timing updating

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import $ from 'jquery';
 
 
-function updateTime {
+function updateTime (){
+  let num = 0;
   setInterval(function() {
     $('#main').html('You have been on this page for ${num} seconds.');
-  })
+    num++;
+  }, 1000);
 }
 
 updateTime();
