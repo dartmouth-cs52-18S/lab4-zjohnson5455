a9a30c712eaaeb825b63939d353455ac5bab2536
zjohnson5455 <zacharyjohnsonri@gmail.com>
2018-04-13 22:43:55 -0400

    adding style

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const env = process.env.NODE_ENV || 'development';
 // set to 'production' or 'development' in your env
 
+const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
+
 module.exports = {
   mode: env,
-  entry: [ 'babel-polyfill', './src' ], // this is where our app lives
+  entry: ['babel-polyfill', './src'], // this is where our app lives
   devtool: 'source-map', // this enables debugging with source in chrome devtools
   module: {
     rules: [
@@ -13,13 +16,35 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
-        { loader: 'babel-loader'}
-      ]
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' },
+        ],
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          finalCSSLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: './index.html',
-})]
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
